b07011a18299b99d412417123cf331163fe0a4d9
fazer1929 <abhishekagrawal8888@gmail.com>
2021-03-06 13:07:38 +0530

    Static Files Setup

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get("/", function (req, res) {
 	res.render("pages/home");
